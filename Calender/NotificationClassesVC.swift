4bb17cf499c97f54c50471c758d8a004c4f25ba9
Deepak Kumar <dkb3194@gmail.com>
2020-07-07 18:15:21 +0530

    DK commited code for veeresh

--- Calender/NotificationClassesVC.swift
+++ Calender/NotificationClassesVC.swift
@@ -7,22 +7,24 @@
 //
 
 import UIKit
-
+import Alamofire
 class NotificationClassesVC: UIViewController, UITableViewDataSource, UITableViewDelegate{
-
-    var classDict =  [LTWEvents]()
-    @IBOutlet weak var notificationClassesTableView : UITableView!
+   
     
 
+    
+    @IBOutlet weak var notificationClassesTableView : UITableView!
+    var classDict : [LTWEvents]!
+    let personType = UserDefaults.standard.string(forKey: "persontyp")
+    let loggedInUserID = UserDefaults.standard.string(forKey: "userID")
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        // Do any additional setup after loading the view.
-    }
-    override func viewWillAppear(_ animated: Bool) {
+        notificationClassesTableView.delegate = self
+        notificationClassesTableView.dataSource = self
         notificationClassesTableView.reloadData()
         print(classDict)
+    }
+    override func viewWillAppear(_ animated: Bool) {
     }
     @IBAction func joinButton(_ sender : UIButton){
         
@@ -31,6 +33,7 @@ class NotificationClassesVC: UIViewController, UITableViewDataSource, UITableVie
         
     }
     @IBAction func unsubscribeButton(_ sender : UIButton) {
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -41,22 +44,33 @@ class NotificationClassesVC: UIViewController, UITableViewDataSource, UITableVie
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let i = classDict[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationClassesCell", for: indexPath) as! NotificationClassesCell
+        if personType == "1"{ //student
+            cell.subscribeUnsubscribeButton.isHidden = false
+            cell.tutorNameLabel.isHidden = false
+            // Attachment for tutor whiteboard image befor tutor name.
+            let attachment = NSTextAttachment()
+            attachment.image = UIImage(named: "Icon awesome-chalkboard-teacher")
+            let attachmentString = NSAttributedString(attachment: attachment)
+            var myString = NSMutableAttributedString(string: i.teacherName) // Veeresh, please add tutor name in this empty space.
+            myString.append(attachmentString)
+            cell.tutorNameLabel.attributedText = myString
+        }else{
+            cell.subscribeUnsubscribeButton.isHidden = true
+            cell.tutorNameLabel.isHidden = true
+            cell.tutorNameLabel.text = ""
+        }
         cell.classTitle.text = i.tittle
         cell.classDate.text = i.key
         cell.grades.text = i.grade
-        cell.timings.text = "\(i.startDate) TO \(i.endDate)"
+        cell.timings.text = "\(i.startDate) To \(i.endDate)"
         cell.subject.text = i.topic
+        
        // cell.tutorNameLabel.text = ""
-        // Attachment for tutor whiteboard image befor tutor name.
-        let attachment = NSTextAttachment()
-        attachment.image = UIImage(named: "Icon awesome-chalkboard-teacher")
-        let attachmentString = NSAttributedString(attachment: attachment)
-        var myString = NSMutableAttributedString(string: " Deepak") // Veeresh, please add tutor name in this empty space.
-        myString.append(attachmentString)
-        cell.tutorNameLabel.attributedText = myString
+       
         return cell
 
     }
+    
 
     
    
